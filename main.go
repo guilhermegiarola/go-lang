022fd223ac20e76d0711ed7a12403c95340d41dc
Guilherme Giarola <guilhermegiarola@outlook.com.br>
2023-02-13 13:33:50 -0300

    feat(tutorial): implemented GET route to specific album

--- main.go
+++ main.go
@@ -31,6 +31,19 @@ func postAlbums(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newAlbum)
 }
 
+func getAlbumByID(c *gin.Context) {
+	id := c.Param("id")
+
+	for _, a := range albums {
+		if a.ID == id {
+			c.IndentedJSON(http.StatusFound, a)
+			return
+		}
+	}
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
+}
+
 // albums slice to seed record album data.
 var albums = []album{
 	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
@@ -43,6 +56,8 @@ func main() {
 	//associate the method to the path
 	router := gin.Default()
 	router.GET("/albums", getAlbums)
+
+	router.GET("/albums/:id", getAlbumByID)
 
 	//uses the POST function also to associate the
 	//post method to the path
